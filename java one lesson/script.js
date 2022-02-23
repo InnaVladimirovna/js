2dba2807d2efa57ce345898e99ba671ad8929c06
InnaVladimirovna <94125147+InnaVladimirovna@users.noreply.github.com>
2022-02-23 20:54:24 +0200

    Add files via upload

---
+++ java one lesson/script.js
@@ -0,0 +1,12 @@
+// let num =+prompt('Ведите число');
+// // alert(num**2);
+
+// let num1=+prompt('число');
+// let num2=(num+num1)/2;
+// alert(num2)
+
+// let num3=+prompt('введите сторону квадрата');
+// let S=(num3*num3);
+// alert(S)
+
+
